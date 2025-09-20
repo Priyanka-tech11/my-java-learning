3102b8b4413a804354dd59749c9d4ef3863ec322
Priyanka <priyankapinka21@gmail.com>
2025-09-20 13:00:12 +0530

    Add files via upload

---
+++ Basics/SumOftwoNum.java
@@ -0,0 +1,17 @@
+class z
+{
+int x=50;
+int y=50;
+ void sum()
+{
+int S=(x+y);
+System.out.println("Sum of two numbers are " +S);
+}
+}
+class main{
+public static void main(String []args)
+{
+z obj=new z();
+obj.sum();
+}
+}
