5dcca17340753bd6da0a8dcba183ccbfa0a98a62
Priyanka <priyankapinka21@gmail.com>
2025-09-21 21:09:11 +0530

    A Java program to swap two variables

---
+++ Basics/swap.java
@@ -0,0 +1,19 @@
+class swap
+{
+public static void main(String []args)
+{
+ int a=5;
+ int b=8;
+System.out.println("before swapping a= " +a);
+System.out.println("before swapping b= " +b);
+int temp=a;
+a=b;
+b=temp;
+System.out.println("after swapping a= " +a);
+System.out.println("after swapping b= " +b);
+
+}
+
+}
+ 
+
