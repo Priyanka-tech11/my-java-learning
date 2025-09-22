d7be3287c76fe64d5f7a0c5756a72a5dc44cf747
Priyanka <priyankapinka21@gmail.com>
2025-09-22 14:01:34 +0530

    Program to right rotate the elements of an array

---
+++ 1D Arrays/Right.java
@@ -0,0 +1,18 @@
+public class Right {
+    public static void main ( String [] args )
+    {
+        int [] arr={1,2,3,4,5};
+        int n=arr.length;
+        int last=arr[4];
+        for (int i=n-1;i>0;i--)
+        {
+            arr[i]=arr[i-1];
+        }
+        arr[0]=last;
+         for(int num:arr)
+         {
+            System.out.println(" " +num);
+         }
+    }
+
+}
