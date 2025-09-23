e47bad90940b7056888ebf482b82c7035ab0b1e6
Priyanka <priyankapinka21@gmail.com>
2025-09-23 20:41:51 +0530

    Program to print the elements of an array present on even position

---
+++ 1D Arrays/even.java
@@ -0,0 +1,12 @@
+public class even {
+    public static void main(String args[])
+    {
+        int arr[]={25,56,78,23,12};
+
+        for (int i=1;i<arr.length;i+=2)
+        {
+            System.out.print( " " + arr[i]);
+        } 
+
+}
+}
