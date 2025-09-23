d8521875522de6d22478d67ce62bef61323295a1
Priyanka <priyankapinka21@gmail.com>
2025-09-23 20:42:33 +0530

    Program to print the elements of an array present on odd position

---
+++ 1D Arrays/oddpos.java
@@ -0,0 +1,11 @@
+public class oddpos {
+    public static void main(String args[])
+    {
+        int arr[]={52,36,45,78,12,58,69,12,41};
+        for (int i=0;i<arr.length;i+=2)
+        {
+            System.out.print(" " + arr[i]);
+        }
+    }
+    
+}
