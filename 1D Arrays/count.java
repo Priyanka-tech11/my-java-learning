92291999503562afbd9b018b1e43ff8ce3c5070a
Priyanka <priyankapinka21@gmail.com>
2025-09-23 20:46:16 +0530

    Program to print the number of elements present in an array

---
+++ 1D Arrays/count.java
@@ -0,0 +1,14 @@
+public class count {
+    public static void main (String [] args)
+    {
+        int arr[]={12,14,15,13,16,1,2,11};
+        int count=0;
+         for (int i=0;i<arr.length;i++)
+         {
+            count ++;
+         }
+         System.out.println("total number of elemnt in array are:" + count);
+        }
+    }
+    
+
