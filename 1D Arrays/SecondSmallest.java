fce228b473f483f2aee1d8e224890a6f407dc7b8
Priyanka <priyankapinka21@gmail.com>
2025-09-24 21:45:00 +0530

    Program to Find 2nd Smallest Number in an array

---
+++ 1D Arrays/SecondSmallest.java
@@ -0,0 +1,19 @@
+public class SecondSmallest {
+    public static void main( String [] args)
+{
+    int arr[]={12,45,78,23,56,89};
+    for (int i=0;i<arr.length;i++){
+        for (int j=1;j<arr.length-i-1;j++)
+        {
+            if(arr[j]<arr[j-1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+            }
+        }
+    }
+    System.out.println("\n2nd Smallest element of an array:"+ arr[1]);
+}    
+}  
+
