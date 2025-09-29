802b61e576ca449d8af20439aa6c96551fae3533
Priyanka <priyankapinka21@gmail.com>
2025-09-29 22:50:29 +0530

    To print odd number from 1 to 20.

---
+++ Basics/oddnumber.java
@@ -0,0 +1,13 @@
+public class oddnumber {
+    public static void main(String [] args)
+    {  System.out.println("odd nummbers are:");
+        for (int i=0;i<=20;i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
