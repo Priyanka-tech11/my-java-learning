3a4f7f2938cf212eab0ef18d78e68c7de0b33147
Priyanka <priyankapinka21@gmail.com>
2025-09-29 22:51:10 +0530

    To print even number from 1 to 20.

---
+++ Basics/evennumber.java
@@ -0,0 +1,13 @@
+public class evennumber {
+    public static void main(String [] args)
+    {  System.out.println("even nummbers are:");
+        for (int i=0;i<=20;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
