9ce747a04e5c0c75a5e77214c38fcc3636170ac7
Priyanka <priyankapinka21@gmail.com>
2025-09-29 22:52:34 +0530

    To convert String to Integer

---
+++ Basics/StrtiInt.java
@@ -0,0 +1,10 @@
+public class StrtiInt {
+    public static void main(String[] arg) {
+        String str = "25689";
+        int num = Integer.parseInt(str);
+        System.out.println("original String:" + str);
+        System.out.println("Converted string:" + num);
+
+    }
+
+}
