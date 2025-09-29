1b0617323eaeaa46092d1464a38198ffb0d202c2
Priyanka <priyankapinka21@gmail.com>
2025-09-29 22:48:18 +0530

    Program to print Ascii value of given Character

---
+++ Basics/Ascii.java
@@ -0,0 +1,8 @@
+public class Ascii {
+    public static void main(String[] args) {
+        char ch = 'A';   
+        int ascii = (int) ch;  
+        System.out.println("Character: " + ch);
+        System.out.println("ASCII value: " + ascii);
+    }
+}
