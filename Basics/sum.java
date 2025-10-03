3561d03a5d79d6638dd95c07b1db31b1ff49dad1
Priyanka <priyankapinka21@gmail.com>
2025-10-03 14:11:17 +0530

    program and compute the sum of the digits of an integer

---
+++ Basics/sum.java
@@ -0,0 +1,16 @@
+public class sum{
+    public static void main (String [] args)
+    {
+        int arr[]={12,14,15,13,16,15,22,11};
+        int sum=0;
+         for (int i=0;i<arr.length;i++)
+         {
+            sum+=arr[i];
+         }
+         System.out.println("sum of all  elemnt in array are:" + sum);
+        }
+    
+    }
+
+    
+
